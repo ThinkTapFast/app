a253cee89cc61856917df1b58171cf811db8b808
khabzox <abdelkabir.ouadoukou@gmail.com>
2025-09-04 10:09:04 +0100

    🐛Fix Docker container name in development script

--- scripts/docker-dev.sh
+++ scripts/docker-dev.sh
@@ -2,4 +2,4 @@
 # Docker development script for Linux/WSL/macOS
 echo "Starting Docker container for development..."
 echo "Current directory: $(pwd)"
-docker run -p 3000:3000 --env-file .env.development.local -v "$(pwd):/app" -v /app/node_modules xymathprepa-dev
+docker run -p 3000:3000 --env-file .env.development.local -v "$(pwd):/app" -v /app/node_modules thinktapfast-app-dev
